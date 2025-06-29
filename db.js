9110b937a4630345b55119c117f054f809de08ef
HungDev17 <hungdev2003@gmail.com>
2025-06-29 22:56:48 +0700

    Update:Chatbot esae

--- db.js
+++ db.js
@@ -15,31 +15,50 @@
 // console.log("✅ MySQL pool đã sẵn sàng");
 
 // module.exports = pool;
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const db = mysql.createConnection({
+//   host: process.env.MYSQLHOST,
+//   port: process.env.MYSQLPORT,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   ssl: {
+//     // Chấp nhận chứng chỉ tự ký (KHÔNG an toàn cho production thật sự)
+//     rejectUnauthorized: false,
+//   },
+//   waitForConnections: true,
+//   connectionLimit: 10, // số lượng connection tối đa
+//   queueLimit: 0, // không giới hạn số lượng request đang chờ
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("❌ Kết nối MySQL thất bại:", err.message);
+//     return;
+//   }
+//   console.log("✅ Đã kết nối MySQL");
+// });
+
+// module.exports = db;
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   ssl: {
-    // Chấp nhận chứng chỉ tự ký (KHÔNG an toàn cho production thật sự)
     rejectUnauthorized: false,
   },
   waitForConnections: true,
-  connectionLimit: 10, // số lượng connection tối đa
-  queueLimit: 0, // không giới hạn số lượng request đang chờ
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Kết nối MySQL thất bại:", err.message);
-    return;
-  }
-  console.log("✅ Đã kết nối MySQL");
-});
-
-module.exports = db;
+module.exports = pool;
